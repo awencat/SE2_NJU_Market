47e93e3c75a4b1b75132a4a284806b5738056ca5
awencat <2364355319@qq.com>
2026-05-28 15:47:27 +0800

    for loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
+    <Loading />
     <router-view/>
   </div>
 </template>
 
 <script>
-
+import Loading from './components/Loading.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
-
+    Loading
   }
 }
 </script>
 
 <style>
 #app {
-
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
+
 
